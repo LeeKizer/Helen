e19aade57fdac51d675548efc26d25d1029f5d2e
LeeKizer <36135684+LeeKizer@users.noreply.github.com>
2024-11-16 19:52:47 -0600

    Update helen_setup.sh

--- helen_setup.sh
+++ helen_setup.sh
@@ -35,7 +35,7 @@ sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian bookwram stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker and plugins
